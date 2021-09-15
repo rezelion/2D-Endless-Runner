39ea137f3e0cb40785cc049e15b6baeca3c001da
rezelion <ahmadspade2014@gmail.com>
2021-09-15 11:05:04 +0700

    Initial commit

---
+++ Assets/Script/CamMoveController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMoveController : MonoBehaviour
+{
+    [Header("Position")]
+    public Transform Player;
+    public float horizontalOffSet;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = transform.position;
+        newPos.x = Player.position.x + horizontalOffSet;
+        transform.position = newPos;
+    }
+}
---
+++ Assets/Script/CharaSoundController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaSoundController : MonoBehaviour
+{
+    private AudioSource audioPlayer;
+    public AudioClip jump;
+    public AudioClip scoreHighlight;
+    public AudioClip fallScream;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+    public void PlayJump() {
+        audioPlayer.PlayOneShot(jump);
+    }
+    public void PlayFalling()
+    {
+        audioPlayer.PlayOneShot(fallScream);
+    }
+    public void PlayScoreHighlight()
+    {
+        audioPlayer.PlayOneShot(scoreHighlight);
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
